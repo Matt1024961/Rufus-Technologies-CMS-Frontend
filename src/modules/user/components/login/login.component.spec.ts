ff13f6b25e1c57968e688395f17b4c962b34f485
Matt Brooks <matt.t.brooks@gmail.com>
2020-02-03 15:18:26 -0700

    quick fix

--- src/modules/user/components/login/login.component.spec.ts
+++ src/modules/user/components/login/login.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { provideMockStore } from '@ngrx/store/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
